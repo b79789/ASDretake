4933a5273e5e6a4e09a82a2e4759fc87a1e6a52e
b79789 <bstacks78@fullsail.edu>
2013-10-08 08:28:04 -0400

    updating cloudant app

--- RWSsiding/RWSsiding/app/_attachments/style/main.js
+++ RWSsiding/RWSsiding/app/_attachments/style/main.js
@@ -1,6 +1,14 @@
 // Brian Stacks 9/30/13
 // ASD--1310
-
+function(doc) {
+  	if(doc._id.substr(0,8) === "estimate"){
+  		emit(doc._id.substr(8), {
+  			"formFirstName": doc.formFirstName,
+  			"formLastName": doc.formLastName,
+  			"formEmail": doc.formEmail,
+  			"formPhone": doc.formPhone
+  		});
+  	
 $("#home").on('pageinit', function () {
     //code needed for home page goes here
 });
@@ -178,3 +186,6 @@ $("#contactUs").on('pageinit', function () {
     //code for contactUs page goes here.
 
 });
+
+}
+};
